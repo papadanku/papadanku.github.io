e448f34fd2ecc5efe1aad8fbf07368f989abd501
papadanku <115061077+papadanku@users.noreply.github.com>
2024-08-03 10:38:08 -0700

    Try Sphinx13 again

--- doc/conf.py
+++ doc/conf.py
@@ -21,4 +21,9 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinxdoc'
+html_theme = 'sphinx13'
+html_theme_path = ['_themes']
+html_css_files = [
+    # 'basic.css',  # included through inheritance from the basic theme
+    'sphinx13.css',
+]
