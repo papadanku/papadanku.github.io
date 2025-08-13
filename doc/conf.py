ccde39022a67a6f9e9d8103702ffc4fd7a7ec730
papadanku <pauldang2@gmail.com>
2025-08-12 21:12:11 -0700

    Update copyright year, switch to furo

--- doc/conf.py
+++ doc/conf.py
@@ -7,7 +7,7 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = "A Shaderboy's Collection"
-copyright = '2024, papadanku'
+copyright = '2025, papadanku'
 author = 'papadanku'
 
 # -- General configuration ---------------------------------------------------
@@ -21,5 +21,5 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'pydata_sphinx_theme'
+html_theme = 'furo'
 html_title = "A Shaderboy's Collection"
